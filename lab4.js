87df95531a983e0f0b81a368f1bd93ee847afed8
sbkhaloof <>
2021-04-14 10:44:37 +0000

    crear script file

---
+++ lab4.js
@@ -0,0 +1,10 @@
+console.log('welcome to my page');
+var userweight= prompt('please enter your weight');
+var userlength= prompt('please enter your length');
+console.log('user weight '+ userweight)
+console.log('user length '+ userlength)
+if (userweight <= 65 && userlength<= 160)
+{console.log('good weight')}
+else
+{console.log('bad weight')}
+ 
