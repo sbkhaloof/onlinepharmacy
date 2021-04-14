80c93aa83f2cd36dd428ea4f96d00bed5e1c75b4
sbkhaloof <>
2021-04-14 10:54:11 +0000

    updating

--- lab4.js
+++ lab4.js
@@ -4,7 +4,7 @@ var userlength= prompt('please enter your length');
 console.log('user weight '+ userweight)
 console.log('user length '+ userlength)
 if (userweight <= 65 && userlength<= 160)
-{console.log('good weight')}
+{alert('good weight')}
 else
-{console.log('bad weight')}
+{alert('bad weight')}
  
