424e7900fe719f8593128ea33ece72ab6a4c92c4
sbkhaloof <>
2021-04-15 11:52:53 +0000

    updating

--- lab5.js
+++ lab5.js
@@ -2,9 +2,16 @@ console.log('this is lab 5');
 var checkupnum= prompt('please enter checkup number');
 var pressure= prompt('please enter your sphagnomanometer reading as number/number');
 for (var i=1;i<= checkupnum;i++)
+{
 if(pressure==='120/80')
 {
-  var image='<img src="https://i.insider.com/5ed178d93f737032db244bf3?width=1136&format=jpeg" alt="perfect blood pressure reading">';
-  document.write(image);
+ var image ='<img src="https://images-na.ssl-images-amazon.com/images/I/61I1BXZTbML._AC_SL1403_.jpg" alt="perfect blood pressure reading"/>';
+ document.write(image)
+  console.log('I have perfect pressure '+ pressure)
 }
-else {console.log('my pressure is '+ pressure);}
+else 
+{
+  var image2='<img src="https://www.glutenfreesociety.org/wp-content/uploads/2019/09/causes-of-high-blood-pressure.jpg" alt="My blood pressure reading"/>';
+  document.write(image2);
+  console.log('my pressure is '+ pressure);}
+} 
