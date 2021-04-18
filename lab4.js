b144543b9bbf0122b44e9a0d6bd29d5765bfd92d
sbkhaloof <>
2021-04-18 10:56:50 +0000

    adding lab6

--- lab4.js
+++ lab4.js
@@ -1,13 +1,11 @@
-console.log('this is lab 5');
+console.log('this is lab 4');
 var userweight= prompt('please enter your weight');
 var userlength= prompt('please enter your length');
 console.log('user weight '+ userweight)
-console.log('welcome to my page');
 console.log('user length '+ userlength)
-if (userweight <= 65 && userlength<= 160)
+if (userweight <= 60 && userlength<= 170)
 {alert('good weight')}
 else
 {alert('bad weight')}
- var x= 5+'7';
-alert(x)
+ 
 
---
+++ lab6.js
@@ -0,0 +1,9 @@
+alert('this is lab 6');
+function tempreture(temp)
+{  
+if(temp==37){alert('your tempreture is perfict');}
+else{alert('you should go to doctor');}
+return temp;}
+alert('here we check your tempreture')
+var x=prompt('please enter your tempreture');
+tempreture(x);
